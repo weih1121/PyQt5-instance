c88dfde0fab5c14507ed4d2d38b91156f672af08
wh <2586898626@qq.com>
2018-03-07 20:31:07 +0800

    客户端与服务端写了一些 问题多多

--- TcpClient/TCPClient.py
+++ TcpClient/TCPClient.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 from TcpClient.client import Ui_Form
+from PyQt5.QtNetwork import QTcpSocket, QHostAddress
 
 class TcpClient(QWidget, Ui_Form):
     def __init__(self):
@@ -8,6 +9,27 @@ class TcpClient(QWidget, Ui_Form):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
 
+        self.tcpSocket = QTcpSocket(self)
+        self.ui.connectButton.clicked.connect(self.buttonConnect)
+        self.tcpSocket.connected.connect(self.sucessConnect)
+        self.ui.sendButton.clicked.connect(self.sendButtonClicked)
+#        self.tcpSocket.Readyread.connect(self.receiveMessage)
+
+    def buttonConnect(self):
+        ip = self.ui.IPLineEdit.text()
+        port = self.ui.portLineEdit.text().toInt()
+        self.tcpSocket.connectToHost(QHostAddress(ip), port)#主动和服务器建立连接
+
+    def sucessConnect(self):
+        self.ui.showText.setText("成功和服务器建立连接")
+
+    def sendButtonClicked(self):
+        str = self.ui.sendText.toPlainText()
+        self.tcpSocket.write(str.toUtf8().data())
+
+    def receiveMessage(self):
+        array = self.tcpSocket.readAll()
+        self.ui.showText.append(array)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- TcpServer/TCPServer.py
+++ TcpServer/TCPServer.py
@@ -1,28 +1,51 @@
 from TcpServer.server import Ui_Form
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5.QtNetwork import *
+from PyQt5.QtNetwork import QTcpSocket, QTcpServer, QHostAddress, QAbstractSocket
+from PyQt5.QtCore import QIODevice
+from TcpClient.TCPClient import TcpClient
 import sys
 
+
 class TcpServer(QWidget, Ui_Form):
+
     def __init__(self):
         super(TcpServer, self).__init__()
         self.ui = Ui_Form()
         self.ui.setupUi(self)
 
-        self.tcpServer = QTcpServer(self) #指定父对象自动回收空间
-        self.tcpSocket = QTcpSocket()
+        self.tcpServer = QTcpServer(self)                           #指定父对象自动回收空间 监听套接字
+        self.tcpSocket = QTcpSocket(self)                           #通信套接字
 
-        self.tcpServer.listen(QHostAddress.Any, 8888)
-        self.tcpServer.newConnection().connect(self.handleNewConnection)
+        self.tcpServer.listen(QHostAddress.Any, 8888)               #any默认绑定当前网卡的所有IP
+        self.tcpServer.newConnection.connect(self.handleNewConnection)
+        self.ui.sendButton.clicked.connect(self.sendMessage)
 
     def handleNewConnection(self):
-        self.socket = self.tcpServer.nextPendingConnection()
-
+        self.tcpSocket = self.tcpServer.nextPendingConnection()       #取出建立好链接的套接字
         #获取对方IP和端口
+        ip = self.tcpSocket.peerAddress
+        port = self.socket.peerPort()
 
+        tmp = "[{IP}:{Port}]".format(IP=ip, Port=port)
+        self.ui.showText.setText(tmp)
+        self.tcpSocket.readyRead.connect(self.showMessage)
+
+    def sendMessage(self):
+        message = self.ui.sendEdit.toPlainText()# 获取编辑区内容
+        self.tcpSocket.write(str)
+
+    def showMessage(self):
+        array = self.tcpSocket.readAll()
+        self.ui.showText.append(array)
+
+    def close(self):
+        self.tcpSocket.disconnectFromHost()
+        self.tcpSocket.close()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     tcpServer = TcpServer()
+    tcpClient = TcpClient()
+    tcpClient.show()
     tcpServer.show()
     sys.exit(app.exec())
